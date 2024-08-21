ff5e0d3addc62047a8afd4662c19fbcef78e4002
iamkkaushik <kodarapukaushik10@gmail.com>
2024-08-21 22:58:06 +0530

    first committtyt

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ import appointmentRouter from "./router/appointmentRouter.js";
 const app = express();
 config({ path: "./config.env" });
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173", process.env.FRONTEND_URL_TWO],
-//     method: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 // app.use(cors()); 
 app.use(cookieParser());
 app.use(express.json());
