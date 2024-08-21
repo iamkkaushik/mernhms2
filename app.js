4ebd6a1a438bc217142c366d04d5abaae8ddbc67
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-08-21 22:45:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import userRouter from "./router/userRouter.js";
 import appointmentRouter from "./router/appointmentRouter.js";
 
 const app = express();
-config({ path: "./config/config.env" });
+config({ path: "./config.env" });
 
 // app.use(
 //   cors({
@@ -19,7 +19,7 @@ config({ path: "./config/config.env" });
 //     credentials: true,
 //   })
 // );
-app.use(cors()); 
+// app.use(cors()); 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
